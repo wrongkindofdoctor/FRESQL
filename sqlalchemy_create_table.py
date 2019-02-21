54147312b80a482dbd3977c4323976189419fc34
Jessica.Liptak <jessica.liptak@noaa.gov>
2019-02-21 14:23:19 -0500

    reorded session initialization, commit, and table creation calls, added an example query and a loop to print the query result

--- sqlalchemy_create_table.py
+++ sqlalchemy_create_table.py
@@ -1,45 +1,39 @@
 #!/usr/bin/python3
 # Create a base class, define classes to map each table to, and create an engine to store a sqlite3 database in your local directory
-# Example adapted from https://www.blog.pythonlibrary.org/2010/09/10/sqlalchemy-connecting-to-pre-existing-databases/
+## Create a base class, define classes to map each table to, and create an engine to store a sqlite3 database in your local directory
+# Example adapted from http://www.lizsander.com/programming/2015/09/08/SQLalchemy-part-2.html
 from sqlalchemy import Column, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-# Describe database tables, then by define classes which will be mapped to those tables. In modern SQLAlchemy, these two tasks are usually performed together, using a system known as Declarative that include directives to describe the actual database table they will be mapped to.
+import logging
 # Create an engine that stores data in the local directory's sqlalchemy_example.db file.
-engine = create_engine('sqlite:///sqlalchemy_example.db')
-# Create the base class
+# echo: logging module used for error messages
+engine = create_engine('sqlite:///sqlalchemy_example.db',echo = True)
+# Create a session to store, and later submit, the database
+Session = sessionmaker(bind = engine)
+session = Session()
+# define basic sqlalchemy table class
 Base = declarative_base()
 # Define a class that the experiment table will be mapped to
-class experiment(Base):
-   __tablename__ = 'new_experiment'
-   # Here we define columns for the experiment name, components, layout, io_layout,
+# Here we define columns for the experiment name, components, layout, io_layout,
    # namelist_paths, data_file_paths
    # Notice that each column is also a normal Python instance attribute.
+class Experiment(Base):
+   __tablename__ = 'experiment' 
    name = Column(String(50), nullable=False, primary_key=True)
-   components = Column(String(500), nullable=False) # comma-separated list of components ‘mom6, shared, ocean_shared’
+   components = Column(String(500), nullable=False)
    layout = Column(String(10), nullable=False)
-   io_layout = Column(String(10), default='1,1') # e.g., '1,1'
-   namelist_paths = Column(String(1000)) # comma-separated list of paths
-   data_file_paths = Column(String(1000)) # comma-separated list of paths
-# define constructor to allow the class to initialize the experiment attributes
-   def __init__(self, name, components,layout,io_layout,namelist_paths, data_file_paths):
-      self.name = name
-      self.components = components
-      self.layout = layout
-      self.io_layout = io_layout
-      self.namelist_paths = namelist_paths
-      self.data_file_paths = data_file_paths
-# define constructor to return printable representation of the experiment object
-   def __repr__(self):
-      return "<Experiment: '%s', '%s', '%s','%s','%s','%s'>" % (self.name, self.components,self.layout, self.io_layout, self.namelist_paths, self.data_file_paths)
-#
-def loadSession():
-   metadata = Base.metadata
-   Session = sessionmaker(bind=engine)
-   session = Session()
-   return session
-#
-if __name__ == "__main__":
-   session = loadSession()
-   res = session.query(experiment).all()
-   print(res[1].name)
+   io_layout = Column(String(10), nullable=False)
+   namelist_paths = Column(String(1000), nullable=False)
+   data_file_paths = Column(String(1000), nullable=False)
+# create the experiment table in the engine
+Base.metadata.create_all(engine)
+# Insert an experiment into the 'experiment' table
+new_experiment = Experiment(name='mom6_double_gyre', components='mom6,shared,ocean_shared',layout='16,16',io_layout='1,1',namelist_paths='/lustre/f2/pdata/gfdl_o/some_namelist.nml', data_file_paths='/lustre/f2/pdata/gfdl_o/input_data/some_netcdf_file.nc')
+# Add and commit 
+session.add(new_experiment)
+session.commit()
+new_exp = session.query(Experiment).filter(Experiment.name == 'mom6_double_gyre')
+for row in new_exp:
+   print('Experiment name is:', row.name,'\n','Experiment layout is:',row.layout)
+
